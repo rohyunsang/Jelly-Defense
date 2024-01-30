c3a264d5a583e3e3bcab3583420fea841a948072
royunsang <82229769+rohyunsang@users.noreply.github.com>
2024-01-30 16:20:23 +0900

    레전더리 슬라임 만들다가 슬라임 정리중

--- Assets/01_Scripts/SlimeSkill/GrassSlimeSkill.cs
+++ Assets/01_Scripts/SlimeSkill/GrassSlimeSkill.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class GrassSlimeSkill : MonoBehaviour
 {
+    public GameObject healingEffect;
+
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Slime"))
         {
-            
+            ISlime slime = other.gameObject.GetComponent<ISlime>();
+            if (slime != null)
+            {
+                float healAmount = slime.MaxHP * 0.3f; // 30% 힐 계산
+                slime.CurrentHP = Mathf.Min(slime.CurrentHP + healAmount, slime.MaxHP); // 현재 체력이 최대 체력을 초과하지 않도록 설정
+            }
         }
     }
 }
