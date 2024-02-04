ad205bd865e6631e68f137a924915da75ff20f82
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-02-04 23:15:37 +0900

    move method to StageManager

--- Assets/01_Scripts/GameManager.cs
+++ Assets/01_Scripts/GameManager.cs
@@ -19,44 +19,8 @@ public class GameManager : MonoBehaviour
         // Set this as the instance and ensure it persists across scenes
         Instance = this;
         DontDestroyOnLoad(this.gameObject);
-
-        // Initialize other components or variables if needed
-        InitializeStageClearStatus();
     }
     #endregion
-
-    [SerializeField]
-    public Dictionary<string, bool> stageClearStatus;
-
-    private void InitializeStageClearStatus()
-    {
-        stageClearStatus = new Dictionary<string, bool>();
-
-        // 일반모드 1-10, 카오스모드 1-10 초기화
-        for (int i = 1; i <= 10; i++)
-        {
-            stageClearStatus.Add("NormalStage" + i, false);
-            stageClearStatus.Add("ChaosStage" + i, false);
-        }
-    }
-
-    // 게임 클리어 하면 이 함수를 실행.
-    public void SetStageCleared(string stageName, bool cleared)
-    {
-        if (stageClearStatus.ContainsKey(stageName))
-        {
-            stageClearStatus[stageName] = cleared;
-        }
-        else
-        {
-            Debug.LogError("Stage name does not exist: " + stageName);
-        }
-    }
-
-    public bool IsStageCleared(string stageName)
-    {
-        return stageClearStatus.TryGetValue(stageName, out bool cleared) && cleared;
-    }
 
     public void ChangeSceneToMain()
     {
