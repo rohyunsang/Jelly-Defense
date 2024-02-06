13d9a407995d0c4063dc31f98dfd53b53325f76b
Roh <82229769+rohyunsang@users.noreply.github.com>
2024-02-07 00:39:51 +0900

    collection

---
+++ Assets/01_Scripts/CollectionSlimeManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectionSlimeManager : MonoBehaviour
+{
+
+    #region SingleTon Pattern
+    public static CollectionSlimeManager Instance { get; private set; }
+    private void Awake()
+    {
+        // If an instance already exists and it's not this one, destroy this one
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+
+        // Set this as the instance and ensure it persists across scenes
+        Instance = this;
+        DontDestroyOnLoad(this.gameObject);
+
+        
+    }
+    #endregion
+
+
+    public GameObject[] SlimeInfoPanels;
+    public int prevIdx = 0;
+
+    public void ShowSlimeInfoPanel(int idx)
+    {
+        SlimeInfoPanels[prevIdx].SetActive(false);
+        SlimeInfoPanels[idx].SetActive(true);
+        prevIdx = idx;
+    }
+
+}
